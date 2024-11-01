df712b5e435b8568648b345fe20ea46da15cc208
Zupicz <jakub.zupko@gmail.com>
2024-11-01 18:23:14 +0100

    hw 02 done

--- domaci_ukol_02.js
+++ domaci_ukol_02.js
@@ -1,12 +1,19 @@
 function decimalToTernary(number) {
-    let remainders = [];
-    intermediateResult = Math.floor(number / 3);
-    remainders.push(number % 3);
+    if (typeof number === 'number') {
+        let remainders = [];
+        let intermediateResult = Math.floor(number / 3);
+        remainders.push(number % 3);
 
-    do {
-        remainders.unshift(intermediateResult % 3);
-        intermediateResult = Math.floor(intermediateResult / 3);
-    } while (intermediateResult > 0);
+        do {
+            remainders.unshift(intermediateResult % 3);
+            intermediateResult = Math.floor(intermediateResult / 3);
+        } while (intermediateResult > 0);
 
-    return Number(remainders.join(''));
+        return Number(remainders.join(''));
+    } else {
+        console.log('Input must be a whole number!');
+    }
 }
+
+x = decimalToTernary(98);
+console.log(x);
