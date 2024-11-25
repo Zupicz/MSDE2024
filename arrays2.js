2c66b2474ba5c5e2418b1cf203a02ae0edf820db
Jakub Zupko <jakub.zupko@gmail.com>
2024-11-25 23:15:18 +0100

    course done

--- arrays2.js
+++ arrays2.js
@@ -94,6 +94,47 @@ const numbersD = [2, 4, 6, 8, 10];
 const areAllEven = numbersD.every(number => {return number % 2 === 0}); // .every returns TRUE if all elements pass the condition
 // console.log(areAllEven);
 
-const numbersE = [1, 3, 5, 7, 8, 9];
+const numbersE = [1, 3, 5, 6, 7, 8, 9];
 const hasOneEvenNumber = numbersD.some(number => {return number % 2 === 0});
-console.log(hasOneEvenNumber);
+// console.log(hasOneEvenNumber);
+
+// Filtering an Array
+// filters through an array and creates new array with elements that meet certain criteria
+const evenNumbers = numbersE.filter(number => number % 2 === 0);
+// console.log(evenNumbers);
+
+let employees2 = [
+    {id: 1, name: 'Alice', role: 'Developer'}, 
+    {id: 2, name: 'Bob', role: 'Designer'}, 
+    {id: 3, name: 'Charlie', role: 'Developer'},
+    {id: 4, name: 'Danielle', role: 'Manager'},
+]; 
+
+const developers = employees2.filter(employee => employee.role === 'Developer');
+// console.log(developers);
+
+// Mapping an Array
+// applies a function on every element of an array and returns a new array made from the altered elements (equivalent of apply() from R)
+const squaredNumbers = numbersD.map(num => num * num);
+// console.log(squaredNumbers);
+
+const characters2 = ['a', 'b', 'c', 'd'];
+const upperCaseCharacters = characters2.map(char => char.toUpperCase());
+// console.log(upperCaseCharacters);
+
+let employees3 = [
+    {id: 1, name: 'Alice', email: 'AliCe@gmail.com'}, 
+    {id: 2, name: 'Steven', email: 'STeVen@gmail.com'}, 
+    {id: 3, name: 'Joe', email: 'JOE@gmail.com'},
+]; 
+
+const employeeEmails = employees3.map(employee => ({
+    ...employee,
+    email: employee.email.toLowerCase()
+}));
+// console.log(employeeEmails);
+
+// Reducing an Array
+// reduce iterates over each element of an array, applying a function that you define, and accumulates the results into a single value
+const sum = numbersE.reduce((accumulator, currentValue) => accumulator + currentValue);
+console.log(sum);
